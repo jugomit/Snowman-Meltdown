80b0fe547f89b420cb86519afc96a2016ab9e2dc
jugom <jugom@gmx.de>
2026-06-18 21:02:56 +0200

    Refactor game logic and enhance guessing logic.

---
+++ game_logic.py
@@ -0,0 +1,53 @@
+import random
+from ascii_art import STAGES
+
+WORDS = ["python", "git", "github", "snowman", "meltdown"]
+
+
+def get_random_word():
+    """Selects a random word from the list."""
+    return WORDS[random.randint(0, len(WORDS) - 1)]
+
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    # Display the snowman stage for the current number of mistakes.
+    print(STAGES[mistakes])
+    # Build a display version of the secret word.
+    display_word = ""
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    print("Word: ", display_word)
+    print("\n")
+
+
+def play_game():
+    secret_word = get_random_word()
+    guessed_letters = []
+    mistakes = 0
+
+    print("Welcome to Snowman Meltdown!")
+    while True:
+        display_game_state(mistakes, secret_word, guessed_letters)
+        guess = input("Guess a letter: ").lower()
+        print("You guessed:", guess)
+        if guess in secret_word:
+            guessed_letters.append(guess)
+        else:
+            mistakes += 1
+
+        word_complete = True
+        for char in secret_word:
+            if char not in guessed_letters:
+                word_complete = False
+                break
+
+        if word_complete == True:
+            print("You guessed the word correctly")
+            break
+
+        if mistakes == 3:
+            print("Snowman melted")
+            break
